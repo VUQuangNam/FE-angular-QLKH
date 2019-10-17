13c1db87f9c4aca177af6807f492465dc47c9b7b
vqn <vuquangnam1507@gmail.com>
2019-10-17 17:33:23 +0700

    done creat

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ProductService } from '../product.service';
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+    selector: 'app-create',
+    templateUrl: './create.component.html',
+    styleUrls: ['./create.component.scss']
+
 })
 export class CreateComponent implements OnInit {
+    registerForm: FormGroup;
 
-  constructor() { }
+    constructor(
+        private fb: FormBuilder,
+        private router: Router,
+        private product: ProductService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.registerForm = this.fb.group({
+            name: [null, [Validators.required]],
+            position: [null, [Validators.required]],
+            office: [null, Validators.required],
+            age: ['', [Validators.required]],
+            startdate: ['', [Validators.required]],
+            salary: ['', [Validators.required]]
+        })
+
+
+    }
+
+    onSubmit() {
+        console.log(this.registerForm);
+        if (this.registerForm.invalid) {
+            return;
+        }
+        console.log(this.registerForm.value);
+        this.product.createProduct(this.registerForm.value)
+            .subscribe(
+                data => {
+                    console.log('succsess');
+                    this.router.navigate(['http://5da3dc1aa6593f001407a03e.mockapi.io/api/v1/']);
+                },
+            );
+    }
 
 }
