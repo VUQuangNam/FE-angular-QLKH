371b72ead30c0ec372ad7d470ddcf373f4329503
vqn <vuquangnam1507@gmail.com>
2019-10-21 11:12:07 +0700

    done delete

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -24,16 +24,24 @@ export class HomePageComponent implements OnInit {
         private route: ActivatedRoute,
         http: HttpClient) {
         http.get<[Product]>('http://5da3dc1aa6593f001407a03e.mockapi.io/api/v1/qlsp').subscribe(res => {
-            this.products = res;
+            this.filteredProduct = res;
         });
         this.config = {
-
             itemsPerPage: 10,
             currentPage: 1,
         };
     }
 
     ngOnInit() {
+        // const id = +this.route.snapshot.paramMap.get('id');
+        // this.productService.getProductById(id).subscribe(() => {
+        //     console.log('success');
+
+        // },
+        //     error => {
+        //         console.log(error);
+        //     });
+
         this.productService.getListProductsByUser().subscribe(
             next => {
                 this.filteredProduct = next;
@@ -42,7 +50,7 @@ export class HomePageComponent implements OnInit {
         );
         this.productService
             .getProducts()
-            .subscribe(next => (this.products = next), error => (this.products = []));
+            .subscribe(next => (this.filteredProduct = next), error => (this.filteredProduct = []));
 
     }
     pageChanged(event) {
@@ -54,17 +62,20 @@ export class HomePageComponent implements OnInit {
         console.log("list " + this.filteredProduct.length);
     }
 
-    //     findProductById(id: number) : Product{
-    //         for(let i =0;i<this.products.l)
-    //         this.products.findIndex
-    // return 
-    //     }
     deletePost(i) {
-        const product = this.products[i];
-        this.productService.deleteProduct(product.id).subscribe(() => {
-            this.products = this.products.filter(t => t.id !== product.id);
-            alert("xoa thanh cong");
-        });
-    }
+        for (let j = 0; j < this.filteredProduct.length - 1; j++) {
+            const product = this.filteredProduct[j];
+            if (product.id === i) {
+                console.log(product);
+                this.productService.deleteProduct(product.id).subscribe(() => {
+                    console.log("delete " + product.id);
+                    const indexOf = this.filteredProduct.indexOf(product);
+                    this.filteredProduct.splice(indexOf, 1);
+                    alert("Delete done");
+                    console.log("Delete");
+                });
+            }
+        }
 
+    }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, max } from 'rxjs/operators';
 import { Product } from './product';
+import { constructDependencies } from '@angular/core/src/di/reflective_provider';
 
 @Injectable({
     providedIn: 'root'
@@ -12,7 +13,6 @@ export class ProductService {
     private readonly API_URL = 'http://5da3dc1aa6593f001407a03e.mockapi.io/api/v1/qlsp';
 
     constructor(private http: HttpClient) {
-
     }
 
     getProducts(count = 10000): Observable<Product[]> {
@@ -20,6 +20,7 @@ export class ProductService {
             map(response => response.filter((product, i) => i < count))
         );
     }
+
     getProductById(id: number): Observable<Product> {
         return this.http.get<Product>(`${this.API_URL}/${(id)}`);
     }
@@ -29,13 +30,20 @@ export class ProductService {
     createProduct(product: Product): Observable<Product> {
         return this.http.post<Product>(this.API_URL, product);
     }
+
+    // deleteProduct(id:number): Observable<Product>{
+    //     console.log(`${this.API_URL}/${(id)}`);
+    //     return this.http.delete<Product>(`${this.API_URL}/${(id)}`);
+    // }
+
     deleteProduct(id: number): Observable<any> {
         return this.http.delete(`${this.API_URL}/${id}`);
+       
     }
+
     updateProduct(product: Product): Observable<Product> {
         return this.http.put<Product>(`${this.API_URL}/${product.id}`, product);
-    } 
-
+    }
     getListProductsByUser(): Observable<any> {
         return this.http.get<any>(this.API_URL);
     }
