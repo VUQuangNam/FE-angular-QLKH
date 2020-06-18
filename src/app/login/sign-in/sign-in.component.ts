cfee59e288d3323cc7bc79778d641c016f5e8e98
VQN <nam.vq@csell.com.vn>
2020-06-18 09:11:41 +0700

    add code

--- src/app/login/sign-in/sign-in.component.ts
+++ src/app/login/sign-in/sign-in.component.ts
@@ -9,7 +9,29 @@ export class SignInComponent implements OnInit {
 
     constructor() { }
 
-    ngOnInit() {
+    ngOnInit() { }
+
+    onclick = () => {
+        try {
+            const value = [];
+            for (let index = 0; index < 10000; index++) {
+                const x = Math.floor((Math.random() * 1000));
+                const check = value.find(a => a === x);
+                if (!check) {
+                    value.push(x);
+                    if (value.length === 480) {
+                        const data = value.map(b => {
+                            if (b < 10) { return '00' + b; }
+                            if (b >= 10 && b < 100) { return '0' + b; }
+                            if (b >= 100) { return + b; }
+                        }).toString();
+                        console.log(data);
+                    }
+                }
+            }
+        } catch (ex) {
+            console.log(ex);
+        }
     }
 
 }
